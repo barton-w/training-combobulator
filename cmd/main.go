cc3750722d2e8c7b2ea760f91fb5437b7e3b106a
Wes Barton <bartonw@gmail.com>
2023-02-09 23:58:38 -0700

    plugging handlers and json result in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"training-combobulator/config"
 	"training-combobulator/dal"
 	"training-combobulator/handlers"
 )
+
+type answers struct {
+	Q1 uint32 `json:"q1"`
+	Q2 uint32 `json:"q2"`
+	Q3 string `json:"q3"`
+	Q4 uint32 `json:"q4"`
+}
 
 func main() {
 	// initialize config
@@ -22,5 +30,49 @@ func main() {
 	}
 	log.Println("connected to database")
 
-	handlers.SomeHandler(dbClient)
+	// The intent is that handlers could be converted to http route handlers,
+	// parsing client query data to fetch various results.
+	// While these handlers are built to answer the questions in the assignment,
+	// they are user and exercise agnostic, and can also provide time based aggregations
+
+	// q1 - In total, how many pounds have these athletes Bench Pressed?
+	answer1, err := handlers.GetExerciseTotalWeight(dbClient, "Bench Press")
+	if err != nil {
+		// don't fatal on these, continue through all answers
+		log.Printf("error: %s", err.Error())
+	}
+
+	// q2 - How many pounds did Barry Moore Back Squat in 2016?
+	aggregates, err := handlers.GetUserYearlyExerciseTotals(dbClient, "Barry", "Moore", "Back Squat")
+	if err != nil {
+		log.Printf("error: %s", err.Error())
+	}
+
+	yearTotals, err := handlers.GetTotalYearlyWeight(aggregates)
+	if err != nil {
+		log.Printf("error: %s", err.Error())
+	}
+	answer2 := yearTotals[2016]
+
+	// q3 - In what month of 2017 did Barry Moore Back Squat the most total weight?
+	maxMonths, err := handlers.GetMaxWeightMonths(aggregates)
+	answer3 := maxMonths[2017]
+
+	// q4 - What is Abby Smith's Bench Press PR weight?
+	answer4, err := handlers.GetUserPR(dbClient, "Abby", "Smith", "Bench Press")
+
+	// respond by logging answers in JSON
+	ans := answers{
+		Q1: answer1,
+		Q2: answer2,
+		Q3: answer3,
+		Q4: answer4,
+	}
+
+	b, err := json.Marshal(ans)
+	if err != nil {
+		log.Fatalf("error marshalling answers to json: %s", err)
+	}
+
+	log.Printf("RESULTS OF THE TRAINING COMBOBULATOR:\n%s\n", string(b))
 }
